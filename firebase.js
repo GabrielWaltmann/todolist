80eac7abd827005d185fa4e6eba52af4e7a18507
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-06-16 17:54:57 -0300

    create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+  
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+apiKey: "AIzaSyDGSmgg0DRMyqij2D1kmaJaTpb7kUS2J20",
+authDomain: "to-do-list-gabriel.firebaseapp.com",
+databaseURL: "https://to-do-list-gabriel-default-rtdb.firebaseio.com",
+projectId: "to-do-list-gabriel",
+storageBucket: "to-do-list-gabriel.appspot.com",
+messagingSenderId: "149369491980",
+appId: "1:149369491980:web:fa5176ad325df4d1579429"
+};
+
+const app = initializeApp(firebaseConfig);
